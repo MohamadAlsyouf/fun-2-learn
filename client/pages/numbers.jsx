9983e01c946d976c9aa66421e12fa353b470167d
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-03-08 11:53:28 -0800

    Created a Numbers component

---
+++ client/pages/numbers.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Numbers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+      numbers: [],
+      playZero: false
+    };
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
