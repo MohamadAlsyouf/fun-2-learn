1dc540ee025f24d9bcd7b5e11452218286c314cb
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-07-01 17:32:17 -0700

    restructured home.jsx code

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -6,9 +6,27 @@ export default class Home extends React.Component {
     return (
       <>
         <div className='row topics'>
-          <a href="#letters"><img src='./images/home/abc-home.png' className='topic-img' onClick={() => { const audio = new Audio('./images/home/letters.mp3'); audio.play(); }}></img></a>
-          <a href="#colors"><img src='./images/home/colors-home.png' className='topic-img' onClick={() => { const audio = new Audio('./images/home/colors.mp3'); audio.play(); }}></img></a>
-          <a href="#numbers"><img src='./images/home/numbers-home.png' className='topic-img' onClick={() => { const audio = new Audio('./images/home/numbers.mp3'); audio.play(); }}></img></a>
+          <a href="#letters">
+            <img
+              src='./images/home/abc-home.png'
+              className='topic-img'
+              onClick={() => { const audio = new Audio('./images/home/letters.mp3'); audio.play(); }}>
+            </img>
+          </a>
+          <a href="#colors">
+            <img
+              src='./images/home/colors-home.png'
+              className='topic-img'
+              onClick={() => { const audio = new Audio('./images/home/colors.mp3'); audio.play(); }}>
+            </img>
+          </a>
+          <a href="#numbers">
+            <img
+              src='./images/home/numbers-home.png'
+              className='topic-img'
+              onClick={() => { const audio = new Audio('./images/home/numbers.mp3'); audio.play(); }}>
+            </img>
+          </a>
         </div>
       </>
     );
